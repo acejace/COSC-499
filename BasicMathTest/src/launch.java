ba0fdfafd65675ad54f63f40519a85f554db6b23
jace lai <acejace@hotmail.com>
2021-09-17 04:06:19 -0700

    Added documentation to inline functions

--- BasicMathTest/src/launch.java
+++ BasicMathTest/src/launch.java
@@ -3,12 +3,26 @@ public class launch {
 	public static int add(int x, int y) {
 		return x+y;
 	}
+	/**
+	 * Supports addition of three integers
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @return
+	 */
     public static int add(int x, int y, int z){
         return x+y+z;
     }
 	public static int multiply(int x, int y) {
 		return x*y;
 	}
+	/**
+	 * Supports multiplication of three integers
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @return
+	 */
     public static int multiply(int x, int y, int z) {
 		return x*y*z;
 	}
