8218a34ff0b1a8795cce4de160397a57f3951905
jace lai <acejace@hotmail.com>
2021-09-17 03:47:40 -0700

    Added test runs on main method

--- BasicMathTest/src/launch.java
+++ BasicMathTest/src/launch.java
@@ -12,4 +12,19 @@ public class launch {
     public static int multiply(int x, int y, int z) {
 		return x*y*z;
 	}
+
+
+	public static void main(String[]args){
+		int test;
+		test = launch.add(10,10); //should return 20
+		System.out.println("10 + 10 = " + test);
+		test = launch.add(10,10,10); //should return 30
+		System.out.println("10 + 10 + 10 = " + test);
+
+		test = launch.multiply(10, 10); //should return 100
+		System.out.println("10 x 10 = " + test);
+		test = launch.multiply(10, 10, 10); //should return 1000
+		System.out.println("10 x 10 x 10 = " + test);
+		
+	}
 }
